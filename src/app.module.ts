e32f10761ebe425a78889bace09c5ba419a75938
许鑫 <xuxin@cloudcc.com>
2023-08-11 10:06:12 +0800

    添加sqlite数据库

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,35 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [AuthModule],
+  imports: [
+    AuthModule,
+    // TypeOrmModule.forRoot({
+    //   database: 'dev.sqlite',
+    //   type: 'sqlite',
+    //   synchronize: true,
+    //   entities: [],
+    // }),
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: `.env.${process.env.NODE_ENV}`,
+    }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: async (config: ConfigService) => {
+        return {
+          type: 'sqlite',
+          database: config.get<string>('DB_NAME'),
+          synchronize: true,
+          entities: [],
+        };
+      },
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
